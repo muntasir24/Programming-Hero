7e014ebb93f835a0eeeb69e45550bfb0a1fb8ca8
moon <azizmuntasirshu@gmail.com>
2025-08-29 19:50:57 +0600

    done frontend

---
+++ Assignment-05/js/home.js
@@ -0,0 +1,15 @@
+// functions
+
+function getVal(id){
+    document.getElementById
+}
+
+
+
+let buttons = document.getElementsByClassName('love-btn');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    console.log('Love button clicked!');
+  });
+}
